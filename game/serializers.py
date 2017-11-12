07ee4e210cdb973be299286bf6e651bf7c40e589
Rin94 <jerad.salinas94@gmail.com>
2017-11-11 18:14:32 -0600

    Creando los servicios de desarrolladora

---
+++ game/serializers.py
@@ -0,0 +1,13 @@
+from game.models import Desarrolladora, Juego
+from rest_framework import serializers
+
+class DesarrolladoraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Desarrolladora
+        fields = ('id','nombre')
+
+
+class JuegoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Juego
+        field =('id','nombre','fecha_publicacion','precio', 'desarrolladora')
---
+++ game/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from game.models import Juego,Desarrolladora
+from game.serializers import JuegoSerializer, DesarrolladoraSerializer
+from rest_framework import mixins
+from rest_framework import generics
+
+# Create your views here.
+
+class ApiDesarrolladora():
+
+    @api_view(['POST'])
+    def create(request):
+        serializer= DesarrolladoraSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Desarrolladora Registrada",status=status.HTTP_201_CREATED)
+        return Response("No se pudo registrar la wea", status=status.HTTP_400_BAD_REQUEST)
+
+    @api_view(['GET'])
+    def get_all_developers(request):
+        queryset=Desarrolladora.objects.all()
+        serializer= DesarrolladoraSerializer(queryset, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    @api_view(['GET', 'PUT', 'DELETE'])
+    def manage_by_id(request, id):
+        if request.method == "GET":
+            return ApiDesarrolladora.get_developer_by_id(request,id)
+        elif request.method == "DELETE":
+            return ApiDesarrolladora.delete_developer(request,id)
+        elif request.method == "PUT":
+            return ApiDesarrolladora.update_developer(request,id)
+
+    def get_developer_or_response(id):
+        try:
+            desarrolladora = Desarrolladora.objects.get(id=id)
+            return desarrolladora
+        except desarrolladora.DoesNotExist:
+            return Response("Desarrolladora no identificada, quizas ya quebraron :(", status=status.HTTP_400_BAD_REQUEST)
+
+
+    def get_developer_by_id(request,id):
+        desarrolladora= ApiDesarrolladora.get_developer_or_response(id)
+
+        if isinstance(desarrolladora,Response):
+            return desarrolladora
+        else:
+            desarrolladora= Desarrolladora.objects.get(id=id)
+            serializer= DesarrolladoraSerializer(desarrolladora)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update_developer(request, id):
+        desarrolladora = ApiDesarrolladora.get_developer_or_response(id)
+
+        if isinstance(desarrolladora,Response):
+            return desarrolladora
+        else:
+            serializer= DesarrolladoraSerializer(desarrolladora, request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response("Desarrolladora Actualizada", status=status.HTTP_200_OK)
+            else:
+                return Response("La desarrolladora no se pudo actualizar: "+serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete_developer(request, id):
+        desarrolladora = ApiDesarrolladora.get_developer_or_response(id)
+
+        if isinstance(desarrolladora,Response):
+            return desarrolladora
+        else:
+            desarrolladora.delete()
+            return Response('Desarrolladora eliminada', status=status.HTTP_200_OK)
+
+
+
+
+
+
